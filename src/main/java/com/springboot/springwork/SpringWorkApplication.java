6637e978331c7ceab5df9ec1b36ae507ab47f98c
daiyuan <846987828@qq.com>
2019-08-28 13:25:55 +0800

    //spring 的学习

---
+++ src/main/java/com/springboot/springwork/SpringWorkApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.springwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkApplication {
+
+    public static void main(String[] args) {
+            //spring 应用启动
+        SpringApplication.run(SpringWorkApplication.class, args);
+    }
+
+}
